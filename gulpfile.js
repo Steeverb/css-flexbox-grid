bb77a5cb4f82dd602aafb47633b82ab3d8372bce
Steve Robinson <steve@excessmarks.com>
2016-12-27 14:05:49 -0500

    adding gulp to project but broken

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,17 @@ var gulp = require('gulp'),
 watch = require('gulp-watch');
 
 gulp.task('default', function() {
-  console.log("Yay")
+  console.log("Call me Sam")
 });
 
 gulp.task('html', function() {
-  console.log("imaging usefulness")
+  console.log("imagine some usefulness in HTML here")
 });
 
 gulp.task('watch', function() {
 
-  watch('index.html')
+  watch('index.html', function() {
+    gulp.start('html');
+  });
 
-})
+});
