da41482b7c2213c9be33a5e7c3f5fb91e02647ee
Steve Robinson <steve@excessmarks.com>
2016-12-31 08:27:58 -0500

    step

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,7 @@
+// index.js
+var lodash = require('flexboxgrid');
+
+var output = flexboxgrid.without([1, 2, 3], 1);
+console.log(output);
+
 /* https://dribbble.com/shots/2339347-Grid-with-CSS-Flexbox-Codepen */
